12eb9e91ae48ffe720ef4443e85f3619aeaa72e0
Lò Văn Anh <anhlo2819@gmail.com>
2024-11-17 15:23:02 +0700

    data hang xuat, panel xuatkho

---
+++ Quanlykhohang/src/Proccess/Hangxuat.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Proccess;
+import java.time.LocalDate;
+public class Hangxuat {
+    private String Manv;
+    private int Sophieuxuat;
+    private LocalDate ngayxuat;
+    private int Mathukho;
+}
